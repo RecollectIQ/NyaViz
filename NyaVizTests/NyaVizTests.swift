ab678eb5ac0196fe0800b9359100a9506a3c769d
Kira-Pgr <aieternal@outlook.com>
2025-12-31 21:47:27 -0600

    Initial Commit

---
+++ NyaVizTests/NyaVizTests.swift
@@ -0,0 +1,17 @@
+//
+//  NyaVizTests.swift
+//  NyaVizTests
+//
+//  Created by Kira Pgr on 12/31/25.
+//
+
+import Testing
+@testable import NyaViz
+
+struct NyaVizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
